9a5e2381fd5dbbf2c5f67fb5ae78660cb36b0ee0
koriavinash1 <avin.kori.re@gmail.com>
2023-01-25 10:29:01 +0000

    sigma initialization change

--- scripts/run.sh
+++ scripts/run.sh
@@ -15,7 +15,7 @@ BINARIZE=${14}
 NAME=${15}
 ITER=${16}
 
-LOGS='/vol/biomedic3/agk21/testEigenSlots2/Testingsss'
+LOGS='/vol/biomedic3/agk21/testEigenSlots2/LOGSTestingsss'
 python /vol/biomedic3/agk21/testEigenSlots2/train.py \
                                             --exp_name $NAME \
                                             --batch_size 16 \
